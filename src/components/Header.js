03c7fc575110654d7268ad3ad151dc7386e39094
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 23:44:35 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -86,7 +86,7 @@ export default class Header extends React.Component {
                                         })}
                                     >
                                       <span>{label}</span>
-                                      <ul class="sub-menu">
+                                      <ul class="subnav">
                                         
                                         <div class="subnav-content">
                                         <a href="#bring">Bring</a>
