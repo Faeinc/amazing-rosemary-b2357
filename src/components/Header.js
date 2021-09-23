01be3ce37c70876295416f0a3ab8bb328d765914
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 23:53:05 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -86,9 +86,9 @@ export default class Header extends React.Component {
                                         })}
                                     aria-haspopup='true' aria-expanded='false'>
                                        
-						<a href="…" aria-haspopup="true" aria-expanded="false">
-							Space Bears
-						</a>
+                                    <a href="…" aria-haspopup="true" aria-expanded="false">
+                                        Space Bears
+                                    </a>
 						<ul>
 								<li><a href="…">Space Bear 6</a></li>
 								<li><a href="…">Space Bear 6 Plus</a></li>
