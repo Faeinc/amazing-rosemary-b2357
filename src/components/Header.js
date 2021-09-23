124a4a9a4204bfd272bd94ab955b6fb17ca00ec2
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 23:52:13 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -83,7 +83,7 @@ export default class Header extends React.Component {
                                         className={classNames('menu-item-submenu', {
                                             'current-menu-item': pageUrl === actionUrl,
                                             'menu-button': actionStyle !== 'link'
-                                        })}
+                                        } aria-haspopup='true' aria-expanded='false')}
                                     >
                                         <li class="has-submenu">
 						<a href="…" aria-haspopup="true" aria-expanded="false">
