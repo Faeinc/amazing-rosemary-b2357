d2120bd8b8f07daecc7c23cea67c9ea5b3cb8808
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 23:10:46 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -85,7 +85,7 @@ export default class Header extends React.Component {
                                             'menu-button': actionStyle !== 'link'
                                         })}
                                     >
-                                      <span>action.</span>
+                                      <span>label</span>
                                     </li>
                                 );
                                 }
