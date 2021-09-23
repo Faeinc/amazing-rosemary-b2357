4bfed2d64c0d827321ff76e574a134356bbbf2f1
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 20:53:33 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -17,7 +17,7 @@ export default class Action extends React.Component {
         const noFollow = _.get(action, 'no_follow');
         let hasChildren = _.get(action, 'children');
         let arrayChildren = Object.entries(hasChildren);
-        console.log("children are:" + arrayChildren[0]);
+        console.log(arrayChildren);
         const attrs = {};
         if (newWindow) {
             attrs.target = '_blank';
