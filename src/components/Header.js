b0d281831cfa24355e35b4d17f139d5b4f852fc3
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 23:12:08 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -86,6 +86,7 @@ export default class Header extends React.Component {
                                         })}
                                     >
                                       <span>{label}</span>
+                                      
                                     </li>
                                 );
                                 }
